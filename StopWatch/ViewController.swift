7ec3ded4424529950d4a3b434a63fc572b6fb565
Ruijie Zhang <Jerrydream@Ruijies-MacBook-Pro.local>
2016-02-10 19:46:44 -0500

    first commit

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var time: UILabel!
+    
+    var count = 0
+    var timer = NSTimer()
+    var startCalled = false
+    
+    func update()
+    {
+        count++
+        time.text = "\(count)"
+    }
+    
+    @IBAction func Pause(sender: AnyObject)
+    {
+        timer.invalidate()
+        startCalled = false
+    }
+    
+    @IBAction func Stop(sender: AnyObject)
+    {
+        timer.invalidate()
+        count = 0
+        time.text = "0"
+        startCalled = false
+    }
+    
+    @IBAction func Start(sender: AnyObject)
+    {
+        if (!startCalled)
+        {
+           timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+           startCalled = true
+        }
+        else
+        {
+            return
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
